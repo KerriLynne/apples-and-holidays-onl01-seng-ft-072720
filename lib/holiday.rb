b9c9ccb485eb49982fd1a4d68923a12e58ef16cf
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-03 20:45:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,8 +71,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+names[]
 holiday_hash.each do |season, holidays|
   holidays.each do |holiday, supplies|
     if supplies.include? "BBQ"
-
+      names << holiday
+    end
 end
